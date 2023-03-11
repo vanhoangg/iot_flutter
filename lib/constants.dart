343101394a9a600dfd8f48250901392636c8cfc4
Biv <kudoshjn70@gmail.com>
2023-03-11 21:00:13 +0700

    Thay doi mau den thanh mau trang

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF0C9869);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
-
 const double kDefaultPadding = 20.0;
+const nhiColor = Color(0xffFFFFF);
